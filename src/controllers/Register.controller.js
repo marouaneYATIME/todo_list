a2497d11d1052690d6c568ead8d7330f7fd70c00
marouaneYATIME <marwaneyatime14@gmail.com>
2023-08-18 23:21:36 +0200

    Registration page

--- src/controllers/Register.controller.js
+++ src/controllers/Register.controller.js
@@ -1,8 +1,9 @@
 import { validationResult } from "express-validator";
 import { jsonGenerate } from "../utils/helpers.js";
-import { StatusCode } from "../utils/constants.js";
+import { StatusCode, JWT_TOKEN_SECRET } from "../utils/constants.js";
+import User from "../models/User.js";
 import bcrypt from 'bcrypt';
-
+import Jwt from 'jsonwebtoken';
 
 const Register =async (req,res) => {
 
@@ -14,9 +15,33 @@ const Register =async (req,res) => {
         const salt= await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, salt);
 
-        password = hashPassword;
+        //verifier si le compte existe deja !
+        const userExist=await User.findOne({ $or: [{
+            email:email
+        },{
+            username : username
+        }]});
 
+        if(userExist){
+            return res.json(jsonGenerate(StatusCode.UNPROCESSABLE_ENTITY,"User or email already exist"))
+        }
         // enregistrer en bd 
+        try {
+            
+            const result=await User.create({
+                name:name,
+                email:email,
+                password:hashPassword,
+                username:username
+            })
+
+
+            const token = Jwt.sign({userId:result._id}, JWT_TOKEN_SECRET);
+
+            res.json(jsonGenerate(StatusCode.SUCCESS,"Registration successfull", {userId:result._id,token:token}))
+        } catch (error) {
+            console.log(error)
+        }
 
     }
     res.json(jsonGenerate(StatusCode.VALIDATION_ERROR,"Validation error", errors.mapped()));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,9 @@
 export const DB_CONNECT="mongodb+srv://Ymaro:maroYuane@cluster0.wnqtv8h.mongodb.net/toto_list?retryWrites=true&w=majority"
 
+export  const JWT_TOKEN_SECRET = "absdgabg";
+
 export const StatusCode={
     SUCCESS:200,
     VALIDATION_ERROR:201,
+    UNPROCESSABLE_ENTITY: 202,
 }
