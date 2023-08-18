d9e8a60e97100a9b2ca555828c854c22f66fc8bc
marouaneYATIME <marwaneyatime14@gmail.com>
2023-08-19 00:41:04 +0200

    Login api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,15 @@
 import express from "express";
 import Register from "../controllers/Register.controller.js";
 import { RegisterSchema } from "../validatonSchema/RegisterSchema.js";
+import Login from "../controllers/Login.controller.js";
+import { LoginSchema } from "../validatonSchema/LoginSchema.js";
 
 
 const apiRoute=express.Router();
 
 
 apiRoute.post('/register',RegisterSchema,Register);
+apiRoute.post('/login',LoginSchema,Login);
 
 
 export default apiRoute;
